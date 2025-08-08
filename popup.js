6680e21cfc4951f88fcdffcfefd402975cf574c8
Akshay Viswanathan <akshayviswa9876@gmail.com>
2025-08-08 19:02:25 +0530

    Updated manifest, added google form for review and general code review

--- popup.js
+++ popup.js
@@ -6,3 +6,8 @@ donate.addEventListener("click",()=>{
 
 
 })
+const googleform=document.getElementById("feedback");
+
+googleform.addEventListener("click",()=>{
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSdIGCTV3Q6xnyQ_Bcak_vte76UmV60XyX0m0EePO4vLQOpuQg/viewform", "_blank");
+})
